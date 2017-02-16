da708d5963f29e1f0c0858be5309732935b938c0
Adrien Poly <adrienpoly@gmail.com>
2017-02-16 16:39:50 +0100

    rake is green I repeat rake is green

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,53 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def edit
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail_id = @cocktail.id
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail_id), notice: 'Dose was successfully created.'
+    else
+      flash[:alert] = 'Invalid dose.'
+      render :new
+    end
+  end
+
+  def update
+    if @dose.update(dose_params)
+      redirect_to @dose, notice: 'Dose was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktails_path, notice: 'Dose was successfully destroyed.'
+  end
+
+  private
+    def set_dose
+      @dose = Dose.find(params[:id])
+    end
+
+    def dose_params
+      # params[:dose][:ingredient_id] = params[:dose][:ingredient].to_i
+      params.require(:dose).permit(:description, :ingredient_id)
+    end
+end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,16 @@ class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses
 
+  def self.names
+    array = []
+    self.all.each { |e| array << e.name }
+    array
+  end
+
+  def self.names_with_index
+    array = []
+    self.all.each { |e| array << [e.name, e.id] }
+    array
+  end
+
 end
