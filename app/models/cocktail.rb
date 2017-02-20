ba80b5a3aa7a621049a379ece88317c6eafc3ec7
Adrien Poly <adrienpoly@gmail.com>
2017-02-20 09:04:02 +0100

    added doses name to edit cocktail form

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,6 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses, dependent: :destroy
   has_attachment :photo
   paginates_per 21
+  # include PgSearch
+  # pg_search_scope :search_cocktail, against:  [name: 'A', instructions: 'B'], using: [:tsearch, :trigram, :dmetaphone]
 end
